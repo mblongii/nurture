9502603c12153fd3d590e90ee1802e8603ed64de
RyanDurling <ryan.p.durling@gmail.com>
2013-02-28 20:08:37 -0800

    Add extra testing for alive?

--- spec/plant_spec.rb
+++ spec/plant_spec.rb
@@ -8,4 +8,29 @@ describe Plant do
   describe 'after creation' do
     its(:alive?) {should be_true}
   end
+
+  describe 'when it has no nutrients' do
+    before {rose.absorb('nutrients', -1)}
+    its(:alive?) {should be_false}
+  end
+
+  describe 'when it is not hydrated' do
+    before {rose.absorb('hydration', -1)}
+    its(:alive?) {should be_false}
+  end
+
+  describe 'when it is infested' do
+    before {rose.absorb('infestation', 1)}
+    its(:alive?) {should be_false}
+  end
+
+  describe 'when it is not infested' do
+    context 'and it is hydrated' do
+      before {rose.absorb('hydration', 1)}
+      context 'and it has nutrients' do
+        before {rose.absorb('nutrients', 1)}
+        its(:alive?) {should be_true}
+      end
+    end
+  end
 end
